37069d5fae2a5df35b8ebc95931336b635afa551
marooncarl <marooncarl@gmail.com>
2023-11-06 16:18:55 -0500

    created main survey component

--- simple-survey/src/index.js
+++ simple-survey/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Survey from './Survey.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<p>Simple Survey</p>);
+root.render(<Survey />);
