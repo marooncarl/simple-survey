1e14577da141b721187087d04facd73f5be936d4
marooncarl <marooncarl@gmail.com>
2023-11-07 18:28:45 -0500

    created css file and made buttons into a column

--- simple-survey/src/index.js
+++ simple-survey/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Survey from './Survey.js';
+import './simple-survey.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Survey />);
