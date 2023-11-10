248992aafc4862e237a7e6cef8a3e842a02d0b32
marooncarl <marooncarl@gmail.com>
2023-11-10 14:19:43 -0500

    added key to survey results in list

--- simple-survey/src/SurveyResults.js
+++ simple-survey/src/SurveyResults.js
@@ -1,7 +1,7 @@
 function SurveyResults(props) {
 	let answers = [];
 	for (let i = 0; i < props.answers.length; i++) {
-		answers.push(<li>{props.headers[i]}: {props.answers[i]}</li>);
+		answers.push(<li key={'ra' + i}>{props.headers[i]}: {props.answers[i]}</li>);
 	}
 	
 	return <div className='results'>
