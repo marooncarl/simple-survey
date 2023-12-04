865eb4bb42873a65b42f99e61c8328b4be594356
marooncarl <marooncarl@gmail.com>
2023-12-04 17:40:08 -0500

    set page title

--- simple-survey/src/index.js
+++ simple-survey/src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom/client';
 import Survey from './Survey.js';
 import './simple-survey.css';
 
+document.title = 'Simple Survey';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Survey />);
